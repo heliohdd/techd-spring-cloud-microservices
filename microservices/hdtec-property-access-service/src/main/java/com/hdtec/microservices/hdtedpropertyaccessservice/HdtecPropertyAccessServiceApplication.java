34ac53d451ae46d06584c7f66d30ff7c239c3b32
Helio Dourado <heliohdd@yahoo.com.br>
2021-02-19 16:06:30 -0300

    Included hdtec-property-access-service microservice

---
+++ microservices/hdtec-property-access-service/src/main/java/com/hdtec/microservices/hdtedpropertyaccessservice/HdtecPropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hdtec.microservices.hdtedpropertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdtecPropertyAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdtecPropertyAccessServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/hdtec-property-access-service/src/test/java/com/hdtec/microservices/hdtedpropertyaccessservice/HdtecPropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdtec.microservices.hdtedpropertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdtecPropertyAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
